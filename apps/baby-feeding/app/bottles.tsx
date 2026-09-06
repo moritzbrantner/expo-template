47cb41167e14506dad5fe3308852a9b72be4db4f
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-06 18:53:32 +0200

    Add bottle tracking page

---
+++ apps/baby-feeding/app/bottles.tsx
@@ -0,0 +1,5 @@
+import { EquipmentTrackerScreen } from '../components/EquipmentTrackerScreen';
+
+export default function BottlesScreen() {
+  return <EquipmentTrackerScreen kind="bottle" title="Bottles" singularLabel="Bottle" icon="🍼" />;
+}
